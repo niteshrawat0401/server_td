ea46658c69bef19e87ded0f7f05af64a9e63d5e8
niteshrawat0401 <niteshrawat0401@gmail.com>
2025-02-24 00:42:50 +0530

    remove id

--- model/todo.js
+++ model/todo.js
@@ -4,7 +4,6 @@ const TodoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
   completed: { type: Boolean, default: false },
-  // user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Todo", TodoSchema);
